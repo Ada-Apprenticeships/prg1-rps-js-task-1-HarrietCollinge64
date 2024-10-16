43834be7b9f5d1c43d527fafca6e81a159dceb87
HarrietCollinge64 <harriet.collinge@ada.ac.uk>
2024-10-16 12:00:43 +0000

    final commit

--- rps.js
+++ rps.js
@@ -1,16 +1,39 @@
 function rockPaperScissors(player1, player2) {
-
+  const options = ['rock', 'paper', 'scissors', 'spock', 'lizard']
   
-   
+
+
+
+ const result = player1 == player2 ? "draw" 
+ : player1 == "rock" && player2 == "paper" ? "player2"
+ : player1 == "rock" && player2 == "scissors" ? "player1"
+ : player1 == "rock" && player2 == "lizard" ? "player1"
+ : player1 == "rock" && player2 == "spock" ? "player2"
+
+ : player1 == "paper" && player2 == "rock" ? "player1"
+ : player1 == "paper" && player2 == "scissors" ? "player2"
+ : player1 == "paper" && player2 == "lizard" ? "player2"
+ : player1 == "paper" && player2 == "spock" ? "player1"
+
+ : player1 == "scissors" && player2 == "rock" ? "player2"
+ : player1 == "scissors" && player2 == "paper" ? "player1"
+ : player1 == "scissors" && player2 == "lizard" ? "player1"
+ : player1 == "scissors" && player2 == "spock" ? "player2"
+
+ : player1 == "spock" && player2 == "rock" ? "player1"
+ : player1 == "spock" && player2 == "scissors" ? "player1"
+ : player1 == "spock" && player2 == "lizard" ? "player2"
+ : player1 == "spock" && player2 == "paper" ? "player2"
+
+ : player1 == "lizard" && player2 == "rock" ? "player2"
+ : player1 == "lizard" && player2 == "scissors" ? "player2"
+ : player1 == "lizard" && player2 == "paper" ? "player1"
+ : player1 == "lizard" && player2 == "spock" ? "player1"
+
+ : null
+ return result
+
 }
-
-
-
-
-
-
-
-
 
 
 // Leave this code here for the automated tests
